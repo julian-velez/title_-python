be6f6f9ec596c5111226197603b218fc49455a46
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-07-10 10:18:17 -0500

    title_´python
    title_´python

---
+++ title/ejemplo.py
@@ -0,0 +1,6 @@
+# Se crea una variable llamada 'aprendiendo' y se le asigna una cadena de texto.
+aprendiendo = 'python es divertido'
+
+# Se utiliza el método .title() sobre la cadena. Este método convierte 
+# la primera letra de cada palabra en mayúscula.
+print(aprendiendo.title())  # Resultado: 'Python Es Divertido'
